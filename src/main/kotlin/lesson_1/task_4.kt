35ce522342432376699f459719aa4b7578321ee2
Soboleff <petr.soboleff@gmail.com>
2025-07-20 22:02:39 +0300

    KS-1-4 task 4

---
+++ src/main/kotlin/lesson_1/task_4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val length: Long = 40_868_600_000L
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("""
+            Length: $length
+            Age: $age
+            Part of day: $partOfDay
+            Seconds: $seconds
+            Part of year: $partOfYear
+            Apogee: $apogee
+    """.trimIndent())
+}
