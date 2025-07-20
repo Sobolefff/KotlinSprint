893523436bd07c1326852ffaecccfc71485d8fcf
Soboleff <petr.soboleff@gmail.com>
2025-07-20 20:51:20 +0300

    KS-1-2 new goals

---
+++ src/main/kotlin/lesson_1/task_2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val orderCount: Int = 50
+    val thankYouMessage: String = "Thank you for your order!"
+
+    println("Количество заказов: $orderCount")
+    println(thankYouMessage)
+
+    var workersCount: Int = 2000
+//    println("Количество работников: $workersCount")
+    workersCount = 1999
+    println("Количество работников: $workersCount")
+
+}
