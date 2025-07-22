f256a708ddb017c41d800c9330195dfbb78eb27a
Soboleff <petr.soboleff@gmail.com>
2025-07-22 10:36:17 +0300

    KS-2-4 refactor variables and calculations

--- src/main/kotlin/lesson_2/task_4.kt
+++ src/main/kotlin/lesson_2/task_4.kt
@@ -1,15 +1,14 @@
 package org.example.lesson_2
 
-const val MINING_BUFF = 0.2
-
 fun main() {
 
+    val miningBuff = 20
     val extractedCrystalOre = 7
     val extractedIronOre = 11
 
     println("""
-        Additional crystalline ore was mined(with buff): ${(extractedCrystalOre * MINING_BUFF).toInt()}
-        Additional iron ore was mined(with buff): ${(extractedIronOre * MINING_BUFF).toInt()}
+        Additional crystalline ore was mined(with buff): ${(extractedCrystalOre * miningBuff / 100)}
+        Additional iron ore was mined(with buff): ${(extractedIronOre * miningBuff / 100)}
     """.trimIndent())
 
 }
