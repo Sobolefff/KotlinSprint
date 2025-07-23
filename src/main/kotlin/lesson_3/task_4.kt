1ed5c5c6ef2bf8d7c59c66e4d83f126903fd1d5e
Soboleff <petr.soboleff@gmail.com>
2025-07-23 20:02:43 +0300

    KS-3-4 task4

---
+++ src/main/kotlin/lesson_3/task_4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var moveNumber = 1
+    var fromMove = "E2"
+    var toMove = "E4"
+
+    println("$fromMove-$toMove;$moveNumber")
+
+    moveNumber++
+    fromMove = "D2"
+    toMove = "D4"
+
+
+    println("$fromMove-$toMove;$moveNumber")
+
+}
