799175d01de81e42ede56fc1ded912fb9fe2b020
Soboleff <petr.soboleff@gmail.com>
2025-07-23 19:28:23 +0300

    KS-3-1 task1

---
+++ src/main/kotlin/lesson_3/task_1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting = "день"
+    val name = "Петр"
+    println("Добрый $greeting, $name!")
+    greeting = "вечер"
+    println("Добрый $greeting, $name!")
+
+}
