081abf20cab7ede13fd6c926f7896647cfa9c025
Soboleff <petr.soboleff@gmail.com>
2025-07-23 19:38:20 +0300

    KS-3-2 task2

---
+++ src/main/kotlin/lesson_3/task_2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $patronymic, $age")
+
+    age = 22
+    lastName = "Сидорова"
+
+    println("$lastName $firstName $patronymic, $age")
+
+
+}
