28cfcfd92ed63b01bc15d0e2513d6b3d3dd99b6c
Soboleff <petr.soboleff@gmail.com>
2025-07-26 15:15:45 +0300

    KS-3-5 сократил код

--- src/main/kotlin/lesson_3/task_5.kt
+++ src/main/kotlin/lesson_3/task_5.kt
@@ -3,10 +3,8 @@ package org.example.lesson_3
 fun main() {
 
     val inputMove = "D2-D4;0"
-    val partsOfMove = inputMove.split(";")
-    val moveNumber = partsOfMove[1].toInt()
-    val fromMove = partsOfMove[0].split("-")[0]
-    val toMove = partsOfMove[0].split("-")[1]
+    val (fromMove, toMove, moveNumber) = inputMove.split("-",";")
+
 
     println("""
         Откуда: $fromMove
