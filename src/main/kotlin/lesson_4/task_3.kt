477947aea2908988384f1ac48cc05d6879e04fb8
Soboleff <petr.soboleff@gmail.com>
2025-07-26 16:45:00 +0300

    KS-4-3 добавил константы, сделал явное сравнение

--- src/main/kotlin/lesson_4/task_3.kt
+++ src/main/kotlin/lesson_4/task_3.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4
 
+const val SUNNY_FOR_LEGUMES = true
+const val TENT_FOR_LEGUMES = true
+const val HUMIDITY_FOR_LEGUMES = 20
+const val SEASON_FOR_LEGUMES = "Зима"
+
 fun main() {
 
     val sunnyToday = true
@@ -11,4 +16,4 @@ fun main() {
 
 }
 
-fun isFavorableForLegumes(sunny: Boolean, tent: Boolean, humidity: Int, season: String) = sunny && tent && humidity == 20 && season == "Зима"
+fun isFavorableForLegumes(sunny: Boolean, tent: Boolean, humidity: Int, season: String) = sunny == SUNNY_FOR_LEGUMES && tent == TENT_FOR_LEGUMES && humidity == HUMIDITY_FOR_LEGUMES && season == SEASON_FOR_LEGUMES
