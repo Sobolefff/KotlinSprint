ed5f7f30dbce35e16bfe9a8a5b0e9709ae86effe
Soboleff <petr.soboleff@gmail.com>
2025-07-27 02:41:22 +0300

    KS-4-5 отформатировал код

--- src/main/kotlin/lesson_4/task_5.kt
+++ src/main/kotlin/lesson_4/task_5.kt
@@ -22,9 +22,14 @@ fun main() {
     val currentWeather = readln().toBoolean()
 
     println(
-        "Корабль допущен к долгосрочному плаванию: ${
-            (currentDamage == DAMAGE && currentCrew in MIN_CREW..MAX_CREW && currentFoodBoxesValue > MIN_FOOD_BOXES_VALUE)
-                    || (currentCrew == 70 && currentWeather == WEATHER_GOOD && currentFoodBoxesValue >= MIN_FOOD_BOXES_VALUE)
+        "Корабль допущен к долгосрочному плаванию: " + "${
+            (currentDamage == DAMAGE &&
+                    currentCrew in MIN_CREW..MAX_CREW &&
+                    currentFoodBoxesValue > MIN_FOOD_BOXES_VALUE) 
+                    ||
+                    (currentCrew == 70 &&
+                            currentWeather == WEATHER_GOOD &&
+                            currentFoodBoxesValue >= MIN_FOOD_BOXES_VALUE)
         }"
     )
 
