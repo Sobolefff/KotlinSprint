58f7761508d65b1802ed0603eeb41c9008cfe995
Soboleff <petr.soboleff@gmail.com>
2025-08-02 18:21:45 +0300

    KS-8-4 оптимизировал код

--- src/main/kotlin/lesson_8/task_4.kt
+++ src/main/kotlin/lesson_8/task_4.kt
@@ -5,12 +5,12 @@ fun main() {
     val soup = arrayOf("мясо", "картофель", "лук", "свекла", "капуста", "морковь", "соль", "перец", "уксус")
     println("Рецепт борща: ${soup.joinToString(", ")}")
     print("Ведите название ингредиента, который вы хотели бы поменять: ")
-    val inputChangeIngredient = readln()
-    if (soup.contains(inputChangeIngredient)) {
-        print("Введите название ингредиента, на который хотите заменить \"$inputChangeIngredient\": ")
+    val inputChangeIngredientIndex = soup.indexOf(readln())
+
+    if (inputChangeIngredientIndex != -1) {
+        print("Введите название ингредиента, на который хотите заменить \"${soup[inputChangeIngredientIndex]}\": ")
         val inputIngredientForChange = readln()
-        val index = soup.indexOf(inputChangeIngredient)
-        soup[index] = inputIngredientForChange
+        soup[inputChangeIngredientIndex] = inputIngredientForChange
         println("Рецепт борща: ${soup.joinToString(", ")}")
     } else println("Такого ингредиента нет в рецепте")
 
