7cb7af3d5a42fc220ef6c1176f21dbcf8cecb4db
Soboleff <petr.soboleff@gmail.com>
2025-08-02 18:25:25 +0300

    KS-8-5 отформатировал код

--- src/main/kotlin/lesson_8/task_5.kt
+++ src/main/kotlin/lesson_8/task_5.kt
@@ -4,11 +4,13 @@ fun main() {
 
     print("Введите количество ингредиентов в рецепте: ")
     val length = readln().toInt()
-    val ingredients = Array(length) {""}
+    val ingredients = Array(length) { "" }
+
     for (i in ingredients.indices) {
-        print("Введите ${i+1} ингредиент: ")
+        print("Введите ${i + 1} ингредиент: ")
         ingredients[i] = readln()
     }
+
     println("Получившийся рецепт ${ingredients.joinToString(", ")}")
 
 }
