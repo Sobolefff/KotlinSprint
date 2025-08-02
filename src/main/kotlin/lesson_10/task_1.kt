b35f0ecc0d7445e4d0ecdbc5e23ea2a0b67405ba
Soboleff <petr.soboleff@gmail.com>
2025-08-02 20:23:15 +0300

    KS-10-1 task1

---
+++ src/main/kotlin/lesson_10/task_1.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_10
+
+fun main() {
+
+    val playerNumber = getRandomNumber()
+    val computerNumber = getRandomNumber()
+    println("Человеку выпало число $playerNumber")
+    println("Компьютеру выпало число $computerNumber")
+
+    val result = when {
+        playerNumber > computerNumber -> "Победило человечество"
+        playerNumber < computerNumber -> "SkyNet победил"
+        else -> "Победила дружба"
+    }
+
+    println(result)
+
+}
+
+fun getRandomNumber(): Int {
+    return (1..9).random()
+}
