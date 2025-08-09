e8ee617e4a315cdcfcfd743c9a1c2f269fd6aa2f
Soboleff <petr.soboleff@gmail.com>
2025-08-09 11:12:25 +0300

    KS-14-1 убрал лишние пробелы перед конструкторами

--- src/main/kotlin/lesson_14/task_1.kt
+++ src/main/kotlin/lesson_14/task_1.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_14
 
-open class Liner (
+open class Liner(
     val name: String,
     val speed: Int = 10,
     val cargoCapacity: Int = 100,
@@ -8,22 +8,24 @@ open class Liner (
     val iceBreaker: Boolean = false,
 ) {
     fun printInfo() {
-        println("""
+        println(
+            """
             Name: $name
             Speed: $speed
             Cargo capacity: $cargoCapacity
             Passengers capacity: $passengersCapacity
             Ice breaker: $iceBreaker
-            """.trimIndent())
+            """.trimIndent()
+        )
         println()
     }
 }
 
-class IceBreaker (
+class IceBreaker(
     name: String,
 ) : Liner(name, speed = 7, cargoCapacity = 70, passengersCapacity = 100, iceBreaker = true)
 
-class CargoShip (
+class CargoShip(
     name: String,
 ) : Liner(name, speed = 8, cargoCapacity = 1000, passengersCapacity = 200, iceBreaker = false)
 
